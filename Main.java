040aa9e8dce7d4b1ddeec7dd64fd0e9dd32204db
Pedro Garcia Vilanova <pedrovilanova2@hotmail.com>
2024-06-27 14:31:18 -0300

    Agora a tokentização está funcionando

--- Main.java
+++ Main.java
@@ -9,16 +9,26 @@ public class Main {
 private static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.printf("Digite o caminho do arquivo:");
-        LeitorTXT txt = new LeitorTXT(scan.nextLine());
+        System.out.printf("Digite o caminho do arquivo: ");
+        LeitorTXT arquivo = new LeitorTXT(scan.nextLine()); // /home/garciavilanovapedro/rep/a
+        ArrayList<String> tokens = new ArrayList<>();
         //
         HashFuncs hashfuncs = new HashFuncs();
         HashMap< String, BiFunction<Double, Double, Double> > mapSimples = hashfuncs.inciarBaseMap();
         HashMap< String, Function<Double[], Double> > mapComplex = hashfuncs.iniciarComplexMap();
 
         
-        ArrayList<String> a = txt.getTxt();
-        a.forEach((e) -> System.out.println(e));
+        ArrayList<String> txt = arquivo.getTxt();
+        txt.forEach((element) -> { 
+            String[] element1 = element.trim().toUpperCase().split("[, ]");
+            for (int i = 0; i < element1.length; i++) {
+                if (!element1[i].equals("")) {
+                    tokens.add(element1[i]);
+                }
+            }
+        });
+
+        System.out.println(tokens);
 
 
         scan.close();
